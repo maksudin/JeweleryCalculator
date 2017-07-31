64d8817e1bb02fdbe7db2ff3e8ae23a8e6d4aac6
maksudin <m.iljushkin2@yandex.ru>
2017-07-31 16:24:22 +0300

    changed names according to guidelines

---
+++ app/src/main/java/com/example/android/jewelery/db/HistoryDbHelper.java
@@ -0,0 +1,8 @@
+package com.example.android.jewelery.db;
+
+/**
+ * Created by milju on 7/31/2017.
+ */
+
+public class HistoryDbHelper {
+}
---
+++ app/src/main/java/com/example/android/jewelery/history/HistoryActivity.java
@@ -0,0 +1,8 @@
+package com.example.android.jewelery.history;
+
+/**
+ * Created by milju on 7/28/2017.
+ */
+
+public class HistoryActivity {
+}
